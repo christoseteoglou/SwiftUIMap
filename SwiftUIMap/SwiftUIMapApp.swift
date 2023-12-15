563e071d5cee0c568e4dc8ba7d2b829ec02e216e
christoseteoglou <itsmeglue@gmail.com>
2023-12-15 13:38:06 +0100

    Initial Commit

---
+++ SwiftUIMap/SwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp.swift
+//  SwiftUIMap
+//
+//  Created by Christos Eteoglou on 2023-12-15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
